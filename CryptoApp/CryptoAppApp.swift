e3d7a8338694e68c10357679d80b7b1ca15baa1f
Murad <Sharudinov001@yandex.ru>
2023-05-20 13:18:13 +0300

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Мурад on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
